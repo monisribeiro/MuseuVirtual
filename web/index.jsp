91418749085e21d08e6ac63bd52d92835d27fe53
monisribeiro <monisribeiro@gmail.com>
2015-10-07 00:05:15 -0300

    pagina da galeria feita e responsiva

--- web/index.jsp
+++ web/index.jsp
@@ -10,13 +10,17 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
 
-        <script type="text/javascript" src="https://code.jquery.com/jquery.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/lib/angular.js"></script>
         <script type="text/javascript" src="js/lib/angular-route.js"></script>
         <script type="text/javascript" src="js/lib/angular-resource.js"></script>
         <script type="text/javascript" src="js/app.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular-animate.js"></script>
+        <script src="//angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.13.4.js"></script>
 
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"
         <link href="bootstrap-theme.css" rel="stylesheet" />
         <link href="bootstrap-theme.min.css" rel="stylesheet" />
         <link href="bootstrap.css" rel="stylesheet" />
--- web/js/app.js
+++ web/js/app.js
@@ -1,12 +1,15 @@
-angular.module('app', ['ngRoute', 'ngResource'])
+angular.module('app', ['ngRoute', 'ngResource', 'ngAnimate', 'ui.bootstrap'])
         .config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/', {
+      when('/main', {
         templateUrl: 'partials/main.html'
       }).
       when('/galeria', {
         templateUrl: 'partials/galeria.html'
+      }).
+      when('/galeria/imagem', {
+        templateUrl: 'partials/visualizacaoImagem.html'
       }).
       when('/desafios', {
         templateUrl: 'partials/desafios.html'
@@ -19,9 +22,6 @@ angular.module('app', ['ngRoute', 'ngResource'])
       }).
       when('/sobreNos', {
         templateUrl: 'partials/sobreNos.html'
-      }).
-      otherwise({
-        redirectTo: '/'
       });
   }])
       .controller('MainController', ['$scope', '$location', function($scope, $location){
@@ -37,6 +37,36 @@ angular.module('app', ['ngRoute', 'ngResource'])
               $location.path("galeria");
           }
   }])
-    .controller('GalleryController', ['$scope', '$http', function($scope, $http){
-  }]);
-
+    .controller('GalleryController', ['$scope', '$modal', function($scope, $modal){
+           $scope.text = "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis."
+           $scope.title = "Thumbnail label"
+           
+          $scope.openImage = function(img, text, title, size){
+            var modalInstance = $modal.open({
+               animation: $scope.animationsEnabled,
+               size:size, 
+               templateUrl: 'myModalContent.html', 
+               controller: 'ModalController', 
+               resolve: {
+                   image: function(){
+                       return img;
+                   },
+                   text: function(){
+                       return text;
+                   },
+                   title: function(){
+                       return title;
+                   }
+               }
+             });
+           };
+   }])
+   .controller('ModalController', ['$scope', 'image', '$modalInstance', 'text', 'title', function($scope, image, $modalInstance, text, title){
+           $scope.title = title;
+           $scope.image = image;
+          $scope.text = text;
+          
+          $scope.closeModal = function(){
+                $modalInstance.dismiss('closeModal');
+           };
+   }]);
